d261d63649db25aa01ae5d009c3f0b4669498bca
Remi Santos <remisantos@gmail.com>
2022-08-26 00:58:20 +0100

    Another Try

--- flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
+++ flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
@@ -1,8 +1,20 @@
 package com.challenge.flightinfo.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 //@EnableWebSecurity
 //@Configuration
-public class SecurityConfig {// extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception{
+    http.authorizeRequests().antMatchers("/").permitAll();
+  }
+}
 
 //  @Override
 //  protected void configure(HttpSecurity http) throws Exception {
@@ -35,4 +47,4 @@ public class SecurityConfig {// extends WebSecurityConfigurerAdapter {
 //    return source;
 //  }
 
-}
+
