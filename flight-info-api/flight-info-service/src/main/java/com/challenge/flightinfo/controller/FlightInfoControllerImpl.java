f8f27866709048dc2341013e2410c779f497e209
Remi Santos <remisantos@gmail.com>
2022-08-25 22:23:21 +0100

    SSL Once Again

--- flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/controller/FlightInfoControllerImpl.java
+++ flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/controller/FlightInfoControllerImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import com.challenge.flightinfo.model.JsonResponse;
 import com.challenge.flightinfo.model.JsonResponseBuilder;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/flight-info")
 public class FlightInfoControllerImpl implements FlightInfoController {
 
