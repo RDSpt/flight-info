bcdac68506b63d1a5a2bacbc5d9df49411a29ed5
Remi Santos <remisantos@gmail.com>
2022-08-25 19:57:38 +0100

    Heroku setup (SSl issues)

--- flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
+++ flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
@@ -1,29 +1,37 @@
 package com.challenge.flightinfo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 //@EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//  @Bean
-//  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//    http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
-//    http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated()).httpBasic(withDefaults());
-//    return http.build();
-//  }
-//
-//  @Bean
-//  CorsConfigurationSource corsConfigurationSource() {
-//    CorsConfiguration configuration = new CorsConfiguration();
-//    configuration.setAllowedOrigins(Arrays.asList("*","https://flight-info-web.herokuapp.com/", "https://flight-info-api.herokuapp.com/"));
-//    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
-//    configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-//      "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
-//      "Cache-Control", "Content-Type"));
-//    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//    source.registerCorsConfiguration("/**", configuration);
-//    return source;
-//  }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+  }
+
+  //  @Bean
+  //  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  //    http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+  //    http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated()).httpBasic(withDefaults());
+  //    return http.build();
+  //  }
+  //
+  //  @Bean
+  //  CorsConfigurationSource corsConfigurationSource() {
+  //    CorsConfiguration configuration = new CorsConfiguration();
+  //    configuration.setAllowedOrigins(Arrays.asList("*","https://flight-info-web.herokuapp.com/", "https://flight-info-api.herokuapp
+  //    .com/"));
+  //    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+  //    configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+  //      "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+  //      "Cache-Control", "Content-Type"));
+  //    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+  //    source.registerCorsConfiguration("/**", configuration);
+  //    return source;
+  //  }
 
 }
