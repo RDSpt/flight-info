44dbd160e3bba27ae640d48e88403e6ec6b6bce6
Remi Santos <remisantos@gmail.com>
2022-08-25 13:23:00 +0100

    Heroku setup (Fix SSL API ISSUES)

--- flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
+++ flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
     http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated()).httpBasic(withDefaults());
     return http.cors().and().build();
   }
