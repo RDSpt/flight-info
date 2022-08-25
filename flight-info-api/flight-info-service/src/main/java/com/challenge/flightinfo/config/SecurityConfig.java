f6cf0ec06aaba9f49a3f195f9d8f79fe946fb631
Remi Santos <remisantos@gmail.com>
2022-08-25 21:58:58 +0100

    EnvVarible name change

--- flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
+++ flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
@@ -4,39 +4,32 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 //@EnableWebSecurity
 @Configuration
-public class SecurityConfig {
-//  extends
-//} WebSecurityConfigurerAdapter {
-//
-//  @Override
-//  protected void configure(HttpSecurity http) throws Exception {
-//    http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
-//  }
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  //  @Bean
-  //  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-  //    http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
-  //    http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated()).httpBasic(withDefaults());
-  //    return http.build();
-  //  }
-  //
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-      CorsConfiguration configuration = new CorsConfiguration();
-      configuration.setAllowedOrigins(Arrays.asList("*"));
-      configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
-      configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-        "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
-        "Cache-Control", "Content-Type"));
-      final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      source.registerCorsConfiguration("/**", configuration);
-      return source;
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+    configuration.setAllowedHeaders(
+      Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Request-Method",
+        "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type"));
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 
 }
