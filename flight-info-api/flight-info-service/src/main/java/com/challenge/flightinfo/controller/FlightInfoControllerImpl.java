0f546ff648852a33bb22d8f436e0701a733f2e2e
Remi Santos <remisantos@gmail.com>
2022-08-25 17:50:03 +0100

    Heroku setup (Cors issues)

--- flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/controller/FlightInfoControllerImpl.java
+++ flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/controller/FlightInfoControllerImpl.java
@@ -7,10 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +20,6 @@ import com.challenge.flightinfo.model.JsonResponseBuilder;
 
 @RestController
 @RequestMapping("/flight-info")
-@CrossOrigin(methods = {RequestMethod.GET,RequestMethod.OPTIONS})
 public class FlightInfoControllerImpl implements FlightInfoController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(FlightInfoControllerImpl.class);
