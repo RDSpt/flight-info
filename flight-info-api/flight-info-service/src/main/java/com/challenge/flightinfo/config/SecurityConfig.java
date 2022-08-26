facc9663c9abab348701f4b6ac93e52fa818fcb8
Remi Santos <remisantos@gmail.com>
2022-08-26 01:59:40 +0100

    Possible Solution to my problems

--- flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
+++ flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception{
     http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+    http.requiresChannel((channel) -> channel.anyRequest().requiresSecure());
     http.authorizeRequests().antMatchers("/").permitAll();
   }
 }
