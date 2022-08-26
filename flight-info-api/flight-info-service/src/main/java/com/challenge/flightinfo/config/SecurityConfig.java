a147d10cb64e14060e22921ec58d29c4bf8d86a4
Remi Santos <remisantos@gmail.com>
2022-08-26 02:03:49 +0100

    Revert "Another Try"
    This reverts commit 8b0c16d3f845ab5b409ffc80acefa1173ea92281.

--- flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
+++ flight-info-api/flight-info-service/src/main/java/com/challenge/flightinfo/config/SecurityConfig.java
@@ -5,16 +5,46 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
+//@EnableWebSecurity
+//@Configuration
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception{
-    http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
     http.authorizeRequests().antMatchers("/").permitAll();
   }
 }
 
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+//  }
+
+//  @Bean
+//  CorsConfigurationSource corsConfigurationSource() {
+//    CorsConfiguration configuration = new CorsConfiguration();
+//    configuration.setAllowedOrigins(Arrays.asList("*"));
+//    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+//    configuration.setAllowedHeaders(
+//      Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Request-Method",
+//        "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type"));
+//    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", configuration);
+//    return source;
+//  }
+
+//  @Bean
+//  CorsConfigurationSource corsConfigurationSource() {
+//    CorsConfiguration configuration = new CorsConfiguration();
+//    configuration.setAllowedOrigins(Arrays.asList("*"));
+//    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+//    configuration.setAllowedHeaders(
+//      Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Request-Method",
+//        "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type"));
+//    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", configuration);
+//    return source;
+//  }
 
 
